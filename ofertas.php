6e28c2e2fee668d4f99ea63dd92ee269db69cb4c
unknown <danielkvaldez2@gmail.com>
2020-02-22 14:00:38 -0300

    subo

---
+++ ofertas.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+        <meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />
+          <title>Pronto ya</title>
+         <meta name="description" content="Buques - Combustibles - Supermercado - Almacen - Bebidas - Comestibles - Perfumeria">   
+         <meta name="language" content="es" /> 
+                          <meta name="dc.creator" content="danielkvaldez" /> 
+                          <meta name="revisit" content="1 day" /> 
+                          <meta name="description" content= "Puerto Quequen Buenos Aires Argentina " />
+                          <meta name="keywords" content="Buques Combustibles Supermercado Almacen Bazar Panificados " />
+                          <meta name="robots" content="index, follow" />
+                          <meta name="apple-mobile-web-app-capable" content="yes" />
+                          <meta name="apple-mobile-web-app-status-bar-style" content="black" /><!-- or black-traslucent -->
+                          <link rel="apple-touch-startup-image" href="http://www.prontoya.com/pronto152.png" />  
+          <link rel="shortcut icon" href="http://www.prontoya.com/favicon.ico" type="image/x-icon">
+          <link rel="shortcut icon" type="image/png" href="http://www.prontoya.com/pronto16.png" />  
+          <link rel="shortcut icon" type="image/svg+xml" href="http://www.prontoya.com/prontoSVG.svg" /> 
+          <link rel="apple-touch-icon" size="" href="http://www.prontoya.com/pronto152.png" />  
+      
+          <link rel="stylesheet" type="text/css" href="css/fonts.css" >
+          <link rel="stylesheet" type="text/css" href="css/style.css" >
+
+    
+</head> 
+        <nav id='menu'>
+                <input type='checkbox'/>
+                <label>&#8801;<span><img src="img/prontoW.png" width="" height="50px" alt=""/></span></label>
+                <ul>
+                    <li><a href='#'>Comprar - Fco de Haro 5285 - Posadas</a></li> <!--  library/supplylogistics.php -->
+                    <li><a href='ofertas.php'>ver Ofertas</a></li>
+                    <li><a href='index.php'>Contact</a></li>
+                </ul>
+            </nav>
+<div class="ofertasCajon">
+                </br> </br> </br> </br> </br>
+
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT"> MAYO 2019 </span>  </br>
+                    <span class="descripOCT">Reserva Temprano al </span>  </br>
+                    <span class="tituloOCT">3764-634166 </span> 
+                </div>
+                <div class="ofertaCajitaFoto">
+                    <img src="img/pronto152.png" height="150px" alt="" class="fotoCajitaOferta">
+                </div>              
+        </div>
+
+         <div class="ofertaCajita">
+                <div class="ofertaCajitaFoto">
+                    <img src="img/asado.jpg" height="150px" alt="" class="fotoCajitaOferta">
+                </div>
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT"> Domingo 26 </span>  </br>
+                    <span class="descripOCT">ASADO-POLLO-MATAMBRE </span>  </br>
+                    <span class="tituloOCT">PARRILLA  </span> 
+                </div>
+        </div>
+
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT"> PEDIDOS </span>  </br>
+                    <span class="descripOCT">Reserva Temprano al </span>  </br>
+                    <span class="tituloOCT">3764-634166 </span> 
+                </div>
+                <div class="ofertaCajitaFoto">
+                    <img src="img/pronto152.png" height="150px" alt="" class="fotoCajitaOferta">
+                </div>              
+        </div>
+        
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaFoto">
+                    <img src="img/patamuslo.jpg" height="150px" alt="" class="fotoCajitaOferta">
+                </div>
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT">PATAMUSLO </span>  </br>
+                    <span class="descripOCT">X 3 KILOS </span>  </br>
+                    <span class="tituloOCT">$ 198 </span> 
+                </div>
+        </div>
+
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT">2 BRAHMA </span>  </br>
+                    <span class="descripOCT">CERVEZA de Litro X </span>  </br>
+                    <span class="tituloOCT">$ 120 </span> 
+                </div>
+                <div class="ofertaCajitaFoto">
+                    <img src="img/brahma.jpg" height="150px" alt="" class="fotoCajitaOferta">
+                </div>      
+        </div>
+        
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaFoto">
+                    <img src="img/quilmes.jpg" height="150px" alt="" class="fotoCajitaOferta">
+                </div>
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT">2 QUILMES </span>  </br>
+                    <span class="descripOCT">CERVEZA de Litro X </span>  </br>
+                    <span class="tituloOCT">$ 115 </span> 
+                </div>
+        </div>
+
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT">HARINA </span>  </br>
+                    <span class="descripOCT">1 Kilo </span>  </br>
+                    <span class="tituloOCT">$ 25 </span> 
+                </div>
+                <div class="ofertaCajitaFoto">
+                    <img src="img/harinaEstrellaDelParana.png" height="150px" alt="" class="fotoCajitaOferta">
+                </div>     
+        </div>
+
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaFoto">
+                    <img src="img/lecheIlolay.jpg" height="150px" alt="" class="fotoCajitaOferta">
+                </div>
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT">LECHE ILOLAY </span>  </br>
+                    <span class="descripOCT">1L.Larga Vida</span>  </br>
+                    <span class="tituloOCT">$ 55 </span> 
+                </div>
+        </div>
+        
+        <div class="ofertaCajita">      
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT">PAN</span>  </br>
+                    <span class="descripOCT">X 500GR</span>  </br>
+                    <span class="tituloOCT">$ 25 </span> 
+                </div>
+                <div class="ofertaCajitaFoto">
+                    <img src="img/mignon.jpg" height="150px" alt="" class="fotoCajitaOferta">
+                </div>
+        </div>
+        
+        <div class="ofertaCajita">
+                <div class="ofertaCajitaTexto">
+                    <span class="tituloOCT"> PEDIDOS </span>  </br>
+                    <span class="descripOCT">Reservas </span>  </br>
+                    <span class="tituloOCT">3764-634166 </span> 
+                </div>
+                <div class="ofertaCajitaFoto">
+                    <img src="img/pronto152.png" height="150px" alt="" class="fotoCajitaOferta">
+                </div>       
+        </div>
+
+        </br></br></br></br>
+</div>
+        </br></br></br></br>
+</body>
+</html>
