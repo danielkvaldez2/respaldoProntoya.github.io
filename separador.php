33b98d7da54fda7ec603394665a9d0a9324c38a3
unknown <danielkvaldez2@gmail.com>
2020-03-11 01:09:18 -0300

    separador

--- separador.php
+++ separador.php
@@ -3,6 +3,7 @@
     <h1 class="text-danger"><span><img src="img/productos/pronto152.png" height="50px" > </span>  DOMINGOS</h1>
     <img src="img/prontoW.png" width="" height="50px" alt=""/>
     <p class="small">Av. Francisco de Haro 5285 - Posadas Misiones</p>
-    <p class="small"><span><img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3765-059161 - <span><img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3764-634166 </p>
+    <p> <strong> ABIERTO de Lunes a Domingos de 8.00hs. a 0.00hs. de CORRIDO </strong> </p>
+    <p class="small"><strong>PEDIDOS al </strong><span><img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3765-059161 - <span><img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3764-634166 </p>
   </div>
 </div>
--- separadorFooter.php
+++ separadorFooter.php
@@ -6,6 +6,7 @@
     <a href="https://wa.me/543765059161?text=Me%20gustaría%20que%20me%20envien%20" target="_blank" ><img src="img/whatsapp.png"  width="50px" height="50px" alt="" title="comunicate con el Local" ></a>
 
     <p class="small">Av. Francisco de Haro 5285 - Posadas Misiones</p>
-    <p class="small"><span><img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3765-059161 - <span><img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3764-634166 </p>
+    <p> <strong> ABIERTO de Lunes a Domingos de 8.00hs. a 0.00hs. de CORRIDO </strong> </p>
+    <p class="small"> <strong>PEDIDOS al </strong><span> <img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3765-059161 - <span><img src="img/whatsapp.png" width="20px" height="20px"  ></span>  (54)3764-634166 </p>
   </div>
 </div> 
